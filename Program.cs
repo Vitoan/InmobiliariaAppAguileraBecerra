30f1a9e961490287228ad7faa845160a82167f87
Vitoan <119367979+Vitoan@users.noreply.github.com>
2025-08-19 15:00:51 -0300

    configurar EF Core con MySQL (Pomelo) y DbContext en Program.cs

--- Program.cs
+++ Program.cs
@@ -1,27 +1,26 @@
+using InmobiliariaApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// MySQL (Pomelo)
+var cs = builder.Configuration.GetConnectionString("InmobiliariaContext");
+builder.Services.AddDbContext<InmobiliariaContext>(options =>
+    options.UseMySql(cs, ServerVersion.AutoDetect(cs)));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
+app.MapControllerRoute(name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
 app.Run();
